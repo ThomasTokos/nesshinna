020d2141b08c4f47d5c1278399b4a6f2fcf48da3
Haden <haden@thehaden.co>
2017-08-19 15:16:45 -0500

    Start basic work on bot

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const Sequelize = require("sequelize");
+
+class Application {
+  constructor() {
+    this.client = new Discord.Client({
+      disabledEvents: [
+        "TYPING_START", "PRESENCE_UPDATE"
+      ]
+    });
+
+    this.config = require("./config.json");
+
+    this.database = new Sequelize({
+        dialect: "mysql",
+        host: this.config.db.host,
+        username: this.config.db.user,
+        password: this.config.db.pass,
+        database: this.config.db.database,
+        // TODO: logging: [function Function],
+      });
+  }
+
+  get embed(options) {
+    return Object.assign({
+      title: `${this.client.user.username}`,
+      color: 0x0086FF,
+      // TODO: url: `https://`,
+    }, options);
+  }
+}
