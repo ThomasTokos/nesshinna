dbe6bbe046d011b478f9af3cdcaf13102ee14416
Haden <haden@thehaden.co>
2017-08-31 22:00:38 -0500

    fix generic module name, improve ping command and invite command, add reload command

--- modules/generic.js
+++ modules/generic.js
@@ -8,7 +8,7 @@ const Application = require("..");
 
 module.exports = {
   meta: {
-    name: "Command Handler",
+    name: "Utilities",
   },
   listeners: [
     ["message", (message, app) => {
@@ -18,28 +18,62 @@ module.exports = {
   commands: {
     ping: {
       run: async (label, message, args, permission, app) => {
-        await message.reply("ping (test)");
+        const date1 = Date.now();
+
+        const pingMessage = await message.channel.send("🏓 Pong!");
+
+        const date2 = Date.now();
+
+        pingMessage.edit({
+          embed: app.embed({
+            title: pingMessage.content,
+            description: `That took **${date2 - date1}ms**. Average heartbeat ping: **${app.client.ping}ms**.`,
+          }),
+        });
       },
       aliases: ["pingu"],
       dm: true,
     },
     invite: {
       run: async (label, message, args, permission, app) => {
-        await message.reply(`here u go <${await app.client.generateInvite([
-          "KICK_MEMBERS",
-          "BAN_MEMBERS",
-          "ADD_REACTIONS",
-          "VIEW_AUDIT_LOG",
-          "READ_MESSAGES",
-          "SEND_MESSAGES",
-          "MANAGE_MESSAGES",
-          "EMBED_LINKS",
-          "MANAGE_NICKNAMES",
-          "CHANGE_NICKNAME",
-          "MANAGE_ROLES",
-        ])}>`);
+        await message.channel.send({
+          embed: app.embed({
+            title: `Invite ${app.client.user.username}`,
+            description: `Add me to your server: <${await app.client.generateInvite([
+              "KICK_MEMBERS",
+              "BAN_MEMBERS",
+              "ADD_REACTIONS",
+              "VIEW_AUDIT_LOG",
+              "READ_MESSAGES",
+              "SEND_MESSAGES",
+              "MANAGE_MESSAGES",
+              "EMBED_LINKS",
+              "MANAGE_NICKNAMES",
+              "CHANGE_NICKNAME",
+              "MANAGE_ROLES",
+            ])}>`
+          })
+        });
       },
       aliases: [],
+      dm: true,
+    },
+    reload: {
+      run: (label, message, args, permission, app) => {
+        if(permission < app.permissions.GLOBAL_ADMIN)
+          return;
+
+        app.loadModules();
+
+        message.channel.send({
+          embed: app.embed({
+            title: "☑ Reloaded",
+            description: "All modules were successfully reloaded.",
+            color: 0x00FF00,
+          }),
+        });
+      },
+      aliases: ["rl"],
       dm: true,
     },
   },
